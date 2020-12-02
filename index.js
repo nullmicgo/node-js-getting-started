2d9507c7c46272dd21e46158842e78ce2ece7d7f
Nullmicgo <nullmicgo@gmail.com>
2020-12-02 15:13:47 +0800

    update

--- index.js
+++ index.js
@@ -4,17 +4,18 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static('client'));
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-.get("/setCookie", (req, res) => {
-res
-.status(302)
-.cookie(
-"amtoken",
-"iwIIDe5qvxAFDR0Yvjf6bTAT84Q.*AAJTSQACMDIAAlNLABxlcTRZTEQ4bnZ1UGJubk01NzRRcjlWZWRoaW89AAR0eXBlAANDVFMAAlMxAAIwNA..*",
-{ httpOnly: true, secure: true }
-)
-.send("set cookie successfully");
-})
+  .get("/setCookie", (req, res) => {
+    res
+      .cookie(
+        "amtoken",
+        "iwIIDe5qvxAFDR0Yvjf6bTAT84Q.*AAJTSQACMDIAAlNLABxlcTRZTEQ4bnZ1UGJubk01NzRRcjlWZWRoaW89AAR0eXBlAANDVFMAAlMxAAIwNA..*",
+        { httpOnly: true, secure: true }
+      )
+      .redirect(302, '/index.html');
+  })
 .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
